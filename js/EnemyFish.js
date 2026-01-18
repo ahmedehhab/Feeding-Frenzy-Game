571a90817a5ca34f502a38a10498d9f4824edbd1
Khaleddd11 <161937385+Khaleddd11@users.noreply.github.com>
2026-01-19 01:38:18 +0200

    refactor: Enhance EnemyFish class with improved initialization and movement logic

--- js/EnemyFish.js
+++ js/EnemyFish.js
@@ -1,54 +1,45 @@
-class EnemyFish {
-    constructor(x, y, imgSrc, weight, direction, directionVertical) {
-        this.x = x;
-        this.y = y;
-        this.width = 100 * weight;
-        this.height = 60 * weight;
-        this.imgSrc = `./images/Enemies/${direction}/${imgSrc}`;
-        this.direction = direction;
-        this.directionVertical = directionVertical;
-        this.weight = weight;
-        this.speedX = 3 * level;
-        this.speedY = 0.3 * level;
-        this.element = null;
-    }
+import Fish from './Fish.js';
+import { CONFIG } from '../config/config.js';
 
-    createFish() {
-        this.element = document.createElement("img"); // create a new image element
-        this.element.src = this.imgSrc;
-        Object.assign(this.element.style, {
-            position: "absolute",
-            height: `${this.height}px`,
-            width: `${this.width}px`
-        });
-        // Determine starting X position based on direction
-        if (this.direction === "left") {
-            //  start at the far right 
-            this.x = window.innerWidth + this.width;
+class EnemyFish extends Fish {
+    constructor(x, y, imgSrc, weight, direction, directionVertical, level = 1) {
+        // Calculate starting X position based on direction BEFORE calling super
+        const width = 100 * weight;
+        const height = 60 * weight;
+        let startX = x;
+        
+        if (direction === "left") {
+            // Start at the far right
+            startX = CONFIG.CANVAS_WIDTH + width;
         } else {
-            // If swimming Right, start at the left 
-            this.x = -this.width;
+            // Start at the left
+            startX = -width;
         }
 
-        this.element.style.top = `${this.y}px`;
-        this.element.style.left = `${this.x}px`;
-        container.appendChild(this.element);
+        super(startX, y, width, height, `./images/Enemies/${direction}/${imgSrc}`);
+        
+        this.direction = direction;
+        this.directionVertical = directionVertical;
+        this.speedX = 3 * level;
+        this.speedY = 0.3 * level;
     }
 
-    moveFishes() {
-        //left and right movement
-    if (this.direction === "left") {
-        this.x -= this.speedX; 
-    } else {
-        this.x += this.speedX;
+    update() {
+        // Left and right movement
+        if (this.direction === "left") {
+            this.x -= this.speedX;
+        } else {
+            this.x += this.speedX;
+        }
+        
+        // Up and down movement
+        if (this.directionVertical === "top") {
+            this.y -= this.speedY;
+        } else {
+            this.y += this.speedY;
+        }
+        
+        
+        this.render();
     }
-    //up and down movement
-    if (this.directionVertical === "top") {
-        this.y -= this.speedY; 
-    } else {
-        this.y += this.speedY; 
-    }
-    this.element.style.left = `${this.x}px`;
-    this.element.style.top = `${this.y}px`;
-}
 }
